3ab42b244269da82ceaba35bdf1a3c3d4ab58dc6
Stephanie Merle <merle.stephany@gmail.com>
2019-08-12 21:35:29 +0200

    Adjustment and adding note functionality

--- app.js
+++ app.js
@@ -12,28 +12,43 @@
  $(this).toggleClass( 'selected' );
   });
 
+// event listener on KEYPRESS, 13 === enter key
+$('input').keypress(function(event){
+  if(event.which === 13){
+    // record the input value into a variable 
+    var todoNext = $(this).val();
+    // input value and push into the grid element
+    $(".notes").append("<div class=\"note grid-item--width3\">" + "<h5 class=\"card-title\">" + "<i class=\"fas fa-check\"></i>" + todoNext + "<span>" + "<i class=\"fas fa-window-close\">" + "</i>" + "</span>" + "</h5>" + "</div>");
+    //finally clean the input box;
+    $('input[type="text"]').val("");
+    // Reload view
+    $grid.isotope(); 
+  };
+}); 
+
+$("ul").on("click" , "li" , function(){
+  $(this).toggleClass("selected");
+});
+
 // DOM manipulation delete note
-$('span').click(function() { 
+// the target of addListener should be an existing element
+$('.notes').on('click','span',function() { 
   $(this).parent().remove();
   $grid.isotope(); 
 });
 
 // DOM manipulation check note
-$('.fa-check').click(function() { 
+// the target of addListener should be an existing element
+$('.notes').on('click','.fa-check',function() { 
   $(this).toggleClass('checked'); 
+  $grid.isotope();
 });
 
 // DOM manipulation pictures size
 $('.grid-item').click(function() {
   $(this).toggleClass('grid-item');
-// toggle class depending on the screen size
-  if($(window).height() < 760){ 
-  $(this).toggleClass('grid-item--width3');
- }else{
   $(this).toggleClass('grid-item--width2');
- }
- // refresh the masonry view
- $grid.isotope(); 
+  $grid.isotope(); 
 });
 
 // filter items on click event from header buttons
@@ -41,6 +56,8 @@ $('.filter-button-group').on( 'click', 'button', function() {
   var filterValue = $(this).attr('data-filter');
   $grid.isotope({ filter: filterValue });
   });
+
+
 
 // init Masonry to use imagesLoaded
 var $gridm = $('.grid').masonry({
@@ -52,4 +69,6 @@ var $gridm = $('.grid').masonry({
 $gridm.imagesLoaded().progress( function() {
   $gridm.masonry('layout');
 });
+
+
 
